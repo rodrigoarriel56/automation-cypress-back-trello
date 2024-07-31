086228cf5461c474775e78dda3cd3a7a91959aeb
Rodrigo Arriel <44985915+rodrigoarriel56@users.noreply.github.com>
2024-07-31 18:09:57 -0300

    Update testeAPI.cy.js

--- cypress/e2e/testeAPI.cy.js
+++ cypress/e2e/testeAPI.cy.js
@@ -1,3 +1,88 @@
 describe('Teste Técnico Astro - Apenas API', () => {
 
-})
+let quadroID;
+let listaID;
+let cartaoID;
+
+it('Cadastrar um quadro', () => {
+
+    cy.request({
+    method: 'POST',
+    url: 'https://api.trello.com/1/boards/',
+    qs: {
+        name: 'ASTRO TESTE QUADRO',
+        key: key,
+        token: token,
+    },
+    }).then((response) => {
+    expect(response.status).to.eq(200);
+    quadroID = response.body.id;
+    cy.log(`ID do quadro criado: ${response.body.id}`);
+    });
+
+});
+
+it('Cadastrar uma lista', () => {
+
+    cy.request({
+    method: 'POST',
+    url: `https://api.trello.com/1/boards/${quadroID}/lists`,
+    qs: {
+        name: 'ASTRO TESTE LISTA',
+        key: key,
+        token: token,
+    },
+    }).then((response) => {
+    expect(response.status).to.eq(200);
+    listaID = response.body.id;
+    cy.log(`ID da lista criada: ${response.body.id}`);
+    });
+});
+
+it('Cadastrar um cartão', () => {
+
+    cy.request({
+    method: 'POST',
+    url: `https://api.trello.com/1/cards`,
+    qs: {
+        name: 'ASTRO TESTE CARTÃO',
+        idList: listaID,
+        key: key,
+        token: token,
+    },
+    }).then((response) => {
+    expect(response.status).to.eq(200);
+    cartaoID = response.body.id;
+    cy.log(`ID do card criado: ${response.body.id}`);
+    });
+});
+
+it('Excluir um card', () => {
+
+    cy.request({
+    method: 'DELETE',
+    url: `https://api.trello.com/1/cards/${cartaoID}`,
+    qs: {
+        key: key,
+        token: token,
+    },
+    }).then((response) => {
+    expect(response.status).to.eq(200);;
+    });
+});
+
+it('Excluir um quadro', () => {
+
+    cy.request({
+    method: 'DELETE',
+    url: `https://api.trello.com/1/boards/${quadroID}`,
+    qs: {
+        key: key,
+        token: token,
+    },
+    }).then((response) => {
+    expect(response.status).to.eq(200);;
+    });
+});
+
+});
